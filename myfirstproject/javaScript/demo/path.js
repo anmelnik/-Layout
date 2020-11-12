f2236a93cbda21a35eb4ab2554ed077fa46776c9
Anastasia Melnik <amelnik@e1r8p18.unit.ua>
2020-11-12 18:35:26 +0200

    Working on tasks

--- myfirstproject/javaScript/demo/path.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-console.log('Name file: ', path.basename(__filename))
-console.log('Name directory: ', path.dirname(__filename))
-console.log('Разширения файла: ', path.extname(__filename))
-console.log('Parse: ', path.parse(__filename))
-console.log(path.join(__dirname, 'server', 'index.html'))
